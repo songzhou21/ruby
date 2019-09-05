dd22f410b5d8f34183eb6f3b3b5fa9f0ebc17de7
Song Zhou <zhousong1993@gmail.com>
2019-09-05 11:44:10 +0800

    refine flow

--- flow.rb
+++ flow.rb
@@ -1,12 +1,28 @@
+puts "---if---"
+
 if 10 < 20
     puts "yes"
 end
 
-if 10 < 20; puts "yes" end
-if 10 < 20 then puts "yes" end
+if 2 < 1
+  puts "if" 
+else
+  puts "else" 
+end
+
+if 2 < 1
+  puts "if" 
+elsif 1 < 2
+  puts "elsif" 
+else
+  puts "else"
+end
 
 puts "yes" if 10 < 20
 
+#other
+if 10 < 20; puts "yes" end
+if 10 < 20 then puts "yes" end
 
 i = 1
 if i== 1; puts 1
